b41b9dc4242f0e3a2e83f1477d13eeb4c6e921f8
Oleg Kostyuk <cub.uanic@gmail.com>
2010-06-20 02:08:40 +0300

    Release new version, 0.02

--- lib/Catalyst/TraitFor/Model/DBIC/Shortcut.pm
+++ lib/Catalyst/TraitFor/Model/DBIC/Shortcut.pm
@@ -9,11 +9,11 @@ Catalyst::TraitFor::Model::DBIC::Shortcut - shortcuts support for DBIC models
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
